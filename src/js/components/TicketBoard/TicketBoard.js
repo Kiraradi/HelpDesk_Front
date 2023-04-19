20d3a3ae15177592df0cdbeea83c96c99fef0ef7
Anatoliy Katelnitskiy <katel1998@yandex.ru>
2023-04-20 00:04:56 +0300

    add ticketBoard

--- src/js/components/TicketBoard/TicketBoard.js
+++ src/js/components/TicketBoard/TicketBoard.js
@@ -1,10 +1,34 @@
-
+import { SERVERURL } from "../../consts";
+import ChangesPopup from "../ChangesPopup/ChangesPopup";
+import './TicketBoard.css'
 export default class TicketBoard {
     constructor(container) {
         this.container = container;
+        this.addTicketCollback = this.addTicketCollback.bind(this);
     }
 
     drawUI() {
+        const bottonsContainer = document.createElement('div');
+        bottonsContainer.classList.add('bottons-container');
 
+        const addTicketButton = document.createElement('button');
+        addTicketButton.classList.add('addTicket-button');
+        addTicketButton.textContent = 'Добавить тикет';
+        bottonsContainer.appendChild(addTicketButton);
+
+        const ticketList = document.createElement('ul');
+        ticketList.classList.add('ticket-list');
+        bottonsContainer.appendChild(ticketList);
+
+        addTicketButton.addEventListener('click', this.addTicketCollback);
+        this.container.appendChild(bottonsContainer)
+
+    }
+
+    addTicketCollback(e) {
+        console.log('ddd')
+        const changesPopup = new ChangesPopup(this.container, true);
+
+        changesPopup.drawUI();
     }
 }
